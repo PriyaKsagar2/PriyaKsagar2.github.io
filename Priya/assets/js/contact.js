b3b02bf9660f08893092d7a3ddf979195436900a
Priya Kishor Kshirsagar <2019bec134@sggs.ac.in>
2024-08-12 18:00:06 +0530

    updated backend!

--- Priya/assets/js/contact.js
+++ Priya/assets/js/contact.js
@@ -1,31 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM fully loaded and parsed");
+
     const form = document.getElementById('contact-form');
     form.addEventListener('submit', function (event) {
-      event.preventDefault();
-  
-      const serviceID = 'service_1dfc2tv';
-      const templateID = 'template_y094bto';
-      const user_id = 'iAI6zH2Z_0XoirgA7'; 
-      const templateParams = {
-        name: form.name.value,
-        email: form.email.value,
-        subject: form.subject.value,
-        message: form.message.value
-      };
-  
-      emailjs.send(serviceID, templateID, templateParams, user_id)
-        .then(function (response) {
-          console.log('SUCCESS!', response.status, response.text);
-          form.querySelector('.sent-message').style.display = 'block';
-          // Optionally clear specific fields instead of resetting the entire form
-          // form.name.value = '';
-          // form.email.value = '';
-          // form.subject.value = '';
-          // form.message.value = '';
-        }, function (error) {
-          console.error('FAILED...', error);
-          form.querySelector('.error-message').style.display = 'block';
-        });
+        event.preventDefault();
+        console.log("Form submission detected");
+
+        const serviceID = "service_1dfc2tv";
+        const templateID = "template_y094bto";
+        const userID = "iAI6zH2Z_0XoirgA7"; 
+
+        const templateParams = {
+            name: form.name.value,
+            email: form.email.value,
+            subject: form.subject.value,
+            message: form.message.value
+        };
+
+        console.log("Sending email with the following parameters:", templateParams);
+
+        emailjs.send(serviceID, templateID, templateParams, userID)
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+                form.querySelector('.sent-message').style.display = 'block';
+                form.reset();
+            }, function (error) {
+                console.error('FAILED...', error);
+                form.querySelector('.error-message').style.display = 'block';
+            });
     });
-  });
-  
+});
